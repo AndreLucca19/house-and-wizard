eca913fad147a338e9d14ca7ba81171c127e5eb5
André Lucca <157771933+AndreLucca19@users.noreply.github.com>
2025-04-08 08:30:48 -0300

    report

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes");
 const houseRoutes = require("./src/routes/houseRoutes");
+const reportRoutes = require("./src/routes/reportRoutes")
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
 app.use("/api/wizards", wizardRoutes);
 app.use("/api/houses", houseRoutes);
+app.use("/api", reportRoutes)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
